77781d220d7fdd975508da2569eb090efdc1708f
Xiaodong Li <xiaodoli@microsoft.com>
2023-09-06 00:34:28 -0700

    fix openssl version

--- monitoring/pre-req.sh
+++ monitoring/pre-req.sh
@@ -1,8 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 set -x # have bash print command been ran
 set -e # fail if any command fails
 
 sudo apt update
+
+# Determine the location of the installed openssl
+openssl_path=$(which openssl)
+
+# Extract the version number of the installed openssl
+openssl_version=$(openssl version | awk '{print $2}')
+
+# Check if the openssl version is less than 3.1.2
+if [[ "$openssl_version" < "3.1.2" ]]; then
+
+    # Check if openssl is installed via Homebrew/Linuxbrew
+    if [[ $openssl_path == *"/linuxbrew/"* ]]; then
+        echo "OpenSSL installed via Linuxbrew. Upgrading..."
+        brew update
+        brew upgrade openssl
+
+    # Check if openssl is installed via system/apt
+    elif [[ $openssl_path == *"/usr/bin/"* ]]; then
+        echo "OpenSSL installed via system package manager. Upgrading..."
+        sudo apt update
+        sudo apt install openssl
+
+    else
+        echo "Unrecognized OpenSSL installation. Please upgrade manually."
+    fi
+
+    # Print the current version of openssl after the upgrade
+    openssl version
+
+else
+    echo "OpenSSL version is $openssl_version which is >= 3.1.2. No need to upgrade."
+fi
+
 sudo apt install goreleaser
 sudo apt install protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.31.0
